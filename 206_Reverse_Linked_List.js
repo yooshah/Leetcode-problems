2f58c14a5e0a6e4fef1cf80c66730c107b39c71e
yooshah <97394955+yooshah@users.noreply.github.com>
2024-11-29 11:27:42 +0530

    Add files via upload

---
+++ 206_Reverse_Linked_List.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let revNode = null;
+  while (head) {
+    temp = head.next;
+
+    head.next = revNode;
+    head.next = revNode;
+    revNode = head;
+    head = temp;
+  }
+  return revNode;
+};
